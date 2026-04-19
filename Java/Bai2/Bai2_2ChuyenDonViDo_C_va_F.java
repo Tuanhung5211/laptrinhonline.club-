7ed6a539234312eb54ef39749c9e8e14782a026b
Tuanhung5211 <phamtuanhung521@gmail.com>
2026-04-19 08:44:01 +0700

    Add files via upload

---
+++ Java/Bai2/Bai2_2ChuyenDonViDo_C_va_F.java
@@ -0,0 +1,12 @@
+package Bai2;
+
+import java.util.Scanner;
+
+public class Bai2_2ChuyenDonViDo_C_va_F {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+
+        long C=scanner.nextLong();
+        System.out.printf("%.2f",((C*9.0/5)+32));
+    }
+}
