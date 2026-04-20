42b4cd9368680a6702f35eaaddc7eaabcec8702f
Phạm Tuấn Hưng <phamtuanhung521@gmail.com>
2026-04-20 08:42:20 +0700

    Update TimSoNguyenTo.java

--- Java/Trang1/TimSoNguyenTo.java
+++ Java/Trang1/TimSoNguyenTo.java
@@ -20,7 +20,7 @@ public class TimSoNguyenTo_OLD {
             for(long j=a[i];j<=b[i];j++){ //Chay tu a[i] den b[i]
                 if(j<2||(j%2==0&&j!=2)||(j%3==0&&j!=3)) continue; // kiem tra xem co chia het cho 2 va 3 khong, neu co, ket thuc vong lap nay
                 //Moi so nguyen to lon hon 3(Prime >3) deu co dang 6k+-1 (VD: 6*1-1=5;6*2+1=13;...)
-                for(int k=6;k<=sqrt(j)+1;k+=6){
+                for(long k=6;k<=sqrt(j)+1;k+=6){
                     if(j%(k-1)==0||j%(k+1)==0){ //Kiem tra xem co chia het cho so nguyen to khong, neu co check=false
                         check=false;
                         break;
